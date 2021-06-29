650da35f60feea10fdd49718d906409a6381487b
aalmusalhi <85188722+aalmusalhi@users.noreply.github.com>
2021-06-29 19:36:24 +0100

    Update wav2vec_test.py
    Loop over a set of input files and process their predicted transcriptions using the model, saving two different sets of results as .csv files

--- wav2vec_test.py
+++ wav2vec_test.py
@@ -1,4 +1,3 @@
-
 from transformers import Wav2Vec2Tokenizer, Wav2Vec2ForCTC
 from datasets import load_dataset
 import soundfile as sf
@@ -11,65 +10,62 @@ import librosa
 from collections import Counter
 import pandas as pd
 
-# Data import
-# def map_to_array(batch):
-#     speech, _ = sf.read(batch["file"])
-#     batch["speech"] = speech
-#     return batch
-#
-# ds = load_dataset("patrickvonplaten/librispeech_asr_dummy", "clean", split="validation")
-# ds = ds.map(map_to_array)
-# wav_to_save = ds["speech"][10]
-# sf.write("Data/test.wav",wav_to_save, 16000)
-
-transcription_truth = ("NEAR THE FIRE AND THE ORNAMENTS FRED BROUGHT HOME FROM INDIA ON THE MANTLE BOARD").split()
-df = pd.read_csv("Results/test.csv")
-#load model and tokenizer
-
-
-f_name = "test.wav"
-f_dir = "Data/"
-y, s = librosa.load((f_dir + f_name), sr=16000)
-acc_arr = []
-noise = np.arange(0,0.1,0.005)
 np.random.seed(1)
-if f_name not in np.asarray(df['f_name']):
-    tokenizer = Wav2Vec2Tokenizer.from_pretrained("facebook/wav2vec2-base-960h")
-    model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
+word_df = pd.read_csv("Results/word_results.csv")
+accuracy_df = pd.read_csv("Results/accuracy_results.csv")
+inputs_df = pd.read_csv("Data/transcription.csv")
 
-    for i in noise:
-        noisy_wav = np.add(y, np.random.normal(0,i * np.max(y),len(y)))
-        input_values = tokenizer(noisy_wav, return_tensors="pt", padding="longest").input_values  # Batch size 1
-        logits = model(input_values).logits
+tokenizer = Wav2Vec2Tokenizer.from_pretrained("facebook/wav2vec2-base-960h")
+model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
+noise = np.arange(0,0.1,0.005)
+f_dir = "Data/"
+for index,f_name in enumerate(inputs_df["file name"]):
 
-        predicted_ids = torch.argmax(logits, dim=-1)
-        transcription = tokenizer.batch_decode(predicted_ids)
-        transcription = transcription[0].split()
+    print("processing file: ", f_name)
+    y, s = librosa.load((f_dir + f_name), sr=16000)
+    acc_arr = []
+    if f_name not in np.asarray(word_df['f_name']) and f_name not in np.asarray(accuracy_df['f_name']):
+        for i in noise:
+            print("processing noise", str(i))
+            noisy_wav = np.add(y, np.random.normal(0, i * np.max(y), len(y)))
+            input_values = tokenizer(noisy_wav, return_tensors="pt", padding="longest").input_values  # Batch size 1
+            logits = model(input_values).logits
 
-        truth_df = pd.DataFrame.from_dict(Counter(transcription_truth), orient='index').reset_index()
-        truth_df = truth_df.rename(columns={"index": "index", 0: "truth_count"})
+            predicted_ids = torch.argmax(logits, dim=-1)
+            transcription = tokenizer.batch_decode(predicted_ids)
+            transcription = transcription[0].split()
 
-        pred_df = pd.DataFrame.from_dict(Counter(transcription), orient='index').reset_index()
-        pred_df = pred_df.rename(columns={"index": "index", 0: "pred_count"})
+            t = np.asarray(inputs_df.iloc[[index]]["Transcription"])[0].split()
 
-        final_df = truth_df.merge(pred_df, how = "left", on = "index")
-        final_df['pred_count'] = final_df['pred_count'].fillna(0)
+            truth_df = pd.DataFrame.from_dict(Counter(t), orient='index').reset_index()
+            truth_df = truth_df.rename(columns={"index": "index", 0: "truth_count"})
 
-        final_df['noise'] = i
-        final_df['f_name'] = f_name
+            pred_df = pd.DataFrame.from_dict(Counter(transcription), orient='index').reset_index()
+            pred_df = pred_df.rename(columns={"index": "index", 0: "pred_count"})
 
-        # df = pd.concat([df,final_df])
-        acc = (final_df['pred_count'].sum())/(final_df['truth_count'].sum())
-        print(acc)
-        acc_arr.append(acc)
-    plt.plot(noise, acc_arr, "r.")
-    plt.xlabel("Gaussian noise spread")
-    plt.ylabel("Accuracy (ratio of predicted to actual counts)")
-    plt.grid()
-    plt.savefig("Mantle_board_acc.png", dpi=300, bbox_inches = "tight")
+            entry_df = truth_df.merge(pred_df, how = "left", on = "index")
+            entry_df['pred_count'] = entry_df['pred_count'].fillna(0)
 
-    final_name = "Results/test.csv"
-    df.to_csv(final_name, index=False)
+            entry_df['noise'] = i
+            entry_df['f_name'] = f_name
 
-else:
-    print("File has already been processed")
+            word_df = pd.concat([word_df,entry_df])
+            acc = (entry_df['pred_count'].sum())/(entry_df['truth_count'].sum())
+            acc_arr.append(acc)
+
+        # Add in code to append accuracies by file to accuracy_df
+        fname_list = np.repeat(f_name, len(acc_arr))
+
+        # Create the pandas DataFrame
+        accuracy_entry = pd.DataFrame({'f_name' : fname_list,
+                                       'noise' : noise,
+                                       'accuracy' : acc_arr})
+
+        accuracy_df = pd.concat([accuracy_df, accuracy_entry])
+
+        word_df.to_csv("Results/word_results.csv", index=False)
+        accuracy_df.to_csv("Results/accuracy_results.csv", index=False)
+
+    else:
+        print("File", f_name,"has already been processed")
+        continue
